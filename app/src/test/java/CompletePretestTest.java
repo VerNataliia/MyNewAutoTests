671c075e00a6e4cd11af5882aac9e589561db1f6
NataliiaVerba <134256981+NataliiaVerba@users.noreply.github.com>
2024-02-03 21:57:45 +0200

    added gitlab-ci.yml file

--- app/src/test/java/CompletePretestTest.java
+++ app/src/test/java/CompletePretestTest.java
@@ -1,6 +1,8 @@
 import io.qameta.allure.*;
 import org.testng.annotations.Test;
 
+import static io.qameta.allure.Allure.step;
+
 @Epic("Pretest")
 @Feature("Pretest")
 
@@ -8,7 +10,7 @@ public class CompletePretestTest extends A_BaseTest {
     @Test(groups = ("Pretest"), priority = 1, description = "Verify if a student can complete a pretest")
     @Severity(SeverityLevel.BLOCKER)
     @Description("Verify if a student can complete a pretest with random answers (Positive case)")
-    @AllureId("1")
+    @AllureId("2")
 
     public void checkPretestExecution() {
         StudentSignUp.signUpAsStudent(app);
