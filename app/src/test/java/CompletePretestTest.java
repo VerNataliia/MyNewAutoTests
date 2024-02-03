a6ad0baac97036a542bfcb209d74412039629590
NataliiaVerba <134256981+NataliiaVerba@users.noreply.github.com>
2024-02-03 20:57:32 +0200

    sdcsdcs

--- app/src/test/java/CompletePretestTest.java
+++ app/src/test/java/CompletePretestTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 @Feature("Pretest")
 
 public class CompletePretestTest extends A_BaseTest {
-    @Test(groups = ("Pretest"), priority = 1, description = "Verify if a student can complete a pretest567656")
+    @Test(groups = ("Pretest"), priority = 1, description = "Verify if a student can complete a pretest")
     @Severity(SeverityLevel.BLOCKER)
     @Description("Verify if a student can complete a pretest with random answers (Positive case)")
 
