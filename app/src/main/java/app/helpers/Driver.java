4309bff03f179cafa9655958c344f1d050f34847
NataliiaVerba <134256981+NataliiaVerba@users.noreply.github.com>
2024-03-14 15:27:44 +0200

    added gitlab-ci.yml file

--- app/src/main/java/app/helpers/Driver.java
+++ app/src/main/java/app/helpers/Driver.java
@@ -15,6 +15,10 @@ import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+import static java.lang.Thread.sleep;
 
 
 abstract public class Driver {
@@ -24,103 +28,109 @@ abstract public class Driver {
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = false;
         Configuration.screenshots = false;
-        Configuration.headless = true;
+        Configuration.headless = false;
+        Configuration.timeout = 10000;
+
     }
 
-        public static WebDriver currentDriver() {
-            return WebDriverRunner.getWebDriver();
+    public static WebDriver currentDriver() {
+        return WebDriverRunner.getWebDriver();
+    }
+
+    public static void open(String url) {
+        Selenide.open(url);
+    }
+
+    public static void refresh() {
+        Selenide.refresh();
+    }
+
+    public static void executeJs(String script) {
+        JavascriptExecutor js = (JavascriptExecutor) currentDriver();
+        try {
+            js.executeScript(script);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
 
-        public static void open(String url) {
-            Selenide.open(url);
+    public static void waitForUrlContains(String urlChunk) {
+        WebDriverWait wait;
+        wait = new WebDriverWait(currentDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlContains(urlChunk));
+    }
+
+    public static void waitForUrlDoesNotContain(String urlChunk) {
+        int maxTime = 20;
+        while (currentDriver().getCurrentUrl().contains(urlChunk) && maxTime > 0) {
+            wait(1);
+            maxTime--;
         }
+    }
 
-        public static void refresh() {
-            Selenide.refresh();
+    public static void maximize() {
+        currentDriver().manage().window().maximize();
+    }
+
+    public static void changeWindowSize(int width, int height) {
+        currentDriver().manage().window().setSize(new Dimension(width, height));
+    }
+
+    public static void clearCookies() {
+        open(AppConfig.BASE_URL);
+        Selenide.clearBrowserCookies();
+        Selenide.clearBrowserLocalStorage();
+    }
+
+    public static void close() {
+        currentDriver().quit();
+    }
+
+    public static void wait(int seconds) {
+        try {
+            sleep(seconds * 10L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+    }
 
-        public static void executeJs(String script) {
-            JavascriptExecutor js = (JavascriptExecutor)currentDriver();
-            try {
-                js.executeScript(script);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+    public static void takeScreenshot() {
+
+        File scrFile = ((TakesScreenshot) currentDriver()).getScreenshotAs(OutputType.FILE);
+
+        String path = System.getProperty("user.dir") + File.separator + "test-output" + File.separator + "screenshots" + File.separator + " " + "screenshot_" + (new SimpleDateFormat("HHmmssSSS").format(new Date())) + ".png";
+        try {
+            FileUtils.copyFile(scrFile, new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
-        public static void waitForUrlContains(String urlChunk) {
-            WebDriverWait wait;
-            wait = new WebDriverWait(currentDriver(), Duration.ofSeconds(10));
-            wait.until(ExpectedConditions.urlContains(urlChunk));
+    public static List<LogEntry> getBrowserLogs() {
+        LogEntries log = currentDriver().manage().logs().get("browser");
+        return log.getAll();
+    }
+
+    // COOKIES
+
+    public static void addCookie(Cookie cookie) {
+        currentDriver().manage().addCookie(cookie);
+    }
+
+    public static Cookie getCookie(String cookieName) {
+        return currentDriver().manage().getCookieNamed(cookieName);
+    }
+
+    public static void deleteCookie(String cookieName) {
+        currentDriver().manage().deleteCookieNamed(cookieName);
+    }
+
+    public void waitForPageLoad() throws InterruptedException {
+        WebDriverRunner.getWebDriver().manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        String jsReadyState = "return document.readyState.equals('complete')";
+        boolean jsReady = executeJavaScript(jsReadyState);
+        if (!jsReady) {
+            sleep(1000); // Waits an additional second if the page is not ready
         }
-
-        public static void waitForUrlDoesNotContain(String urlChunk) {
-            int maxTime = 20;
-            while(  currentDriver().getCurrentUrl().contains(urlChunk)  && maxTime > 0) {
-                wait(1);
-                maxTime--;
-            }
-        }
-
-        public static void maximize() {
-            currentDriver().manage().window().maximize();
-        }
-
-        public static void changeWindowSize(int width, int height) {
-            currentDriver().manage().window().setSize(new Dimension(width, height));
-        }
-
-        public static void clearCookies() {
-            open(AppConfig.BASE_URL);
-            Selenide.clearBrowserCookies();
-            Selenide.clearBrowserLocalStorage();
-            //Selenide.refresh();
-        }
-
-        public static void close() {
-            currentDriver().quit();
-        }
-
-        public static void wait(int seconds)
-        {
-            try {
-                Thread.sleep(seconds * 10L);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public static void takeScreenshot() {
-
-            File scrFile = ((TakesScreenshot) currentDriver()).getScreenshotAs(OutputType.FILE);
-
-            String path = System.getProperty("user.dir")
-                    + File.separator + "test-output"
-                    + File.separator + "screenshots"
-                    + File.separator + " " + "screenshot_" +  (new SimpleDateFormat("HHmmssSSS").format(new Date())) + ".png";
-            try {
-                FileUtils.copyFile(scrFile, new File(path));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public static List<LogEntry> getBrowserLogs() {
-            LogEntries log = currentDriver().manage().logs().get("browser");
-            return log.getAll();
-        }
-
-        // COOKIES
-
-        public static void addCookie(Cookie cookie) {
-            currentDriver().manage().addCookie(cookie);
-        }
-
-        public static Cookie getCookie(String cookieName) {
-            return currentDriver().manage().getCookieNamed(cookieName);
-        }
-
-        public static void deleteCookie(String cookieName) {
-            currentDriver().manage().deleteCookieNamed(cookieName);
-        }
+    }
 }
