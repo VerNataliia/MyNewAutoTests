f44a60532573208affe6547093e7092b52969c18
NataliiaVerba <134256981+NataliiaVerba@users.noreply.github.com>
2024-03-14 15:14:35 +0200

    added gitlab-ci.yml file

---
+++ app/src/test/java/HelperSeverityLevelInterceptor.java
@@ -0,0 +1,25 @@
+import org.testng.IMethodInstance;
+import org.testng.IMethodInterceptor;
+import org.testng.ITestContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Severity;
+
+public class HelperSeverityLevelInterceptor implements IMethodInterceptor {
+    @Override
+    public List<IMethodInstance> intercept(List<IMethodInstance> methods, ITestContext context) {
+        List<IMethodInstance> result = new ArrayList<>();
+        for (IMethodInstance method : methods) {
+            Severity severity = method.getMethod().getConstructorOrMethod().getMethod().getAnnotation(Severity.class);
+            if (severity != null && (severity.value().equals(SeverityLevel.BLOCKER) || severity.value().equals(SeverityLevel.CRITICAL))) {
+                result.add(method);
+            }
+        }
+        return result;
+    }
+
+}
+
