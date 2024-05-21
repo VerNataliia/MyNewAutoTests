39f8a8ba22ad2576c675d6e1cf43cf599f21f153
NataliiaVerba <134256981+NataliiaVerba@users.noreply.github.com>
2024-05-21 10:23:19 +0300

    new

--- app/src/main/java/app/helpers/Driver.java
+++ app/src/main/java/app/helpers/Driver.java
@@ -29,7 +29,7 @@ public final class Driver {
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = false;
         Configuration.screenshots = false;
-        Configuration.headless = true;
+        Configuration.headless = false;
         Configuration.timeout = 10000;
     }
 
